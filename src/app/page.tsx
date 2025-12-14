5b4580bd1bbd66bf1773a14d7f6082cca2622aab
xvcf <53903394+RafaeloxMC@users.noreply.github.com>
2025-12-14 08:36:23 +0100

    Add react-snowfall

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 "use client";
 import SantaClick from "@/components/SantaClick";
 import { useState } from "react";
+import Snowfall from "react-snowfall";
 
 export default function Home() {
 	const [score, setScore] = useState(0);
 
 	return (
 		<div className="flex flex-col gap-4 items-center justify-center p-16">
+			<Snowfall />
 			<h1 className="text-8xl">Click the Santa</h1>
 			<SantaClick onClick={() => setScore(score + 1)} />
 			<span className="text-4xl">You clicked Santa {score} times!</span>
